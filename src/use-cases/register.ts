4b1bdd01dca676d9b7a6aada136a642f0ef28b46
Fellipe Nogueira Milomem <fellipemilomem@hotmail.com>
2024-02-22 19:38:14 -0300

    Auth

---
+++ src/use-cases/register.ts
@@ -0,0 +1,42 @@
+import { UsersRepository } from '@/repository /users-repository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-erros'
+import { User } from '@prisma/client'
+
+interface RegisterUserCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+interface RegisterUseCaseResponse {
+  user: User
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUserCaseRequest): Promise<RegisterUseCaseResponse> {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    // const prismaUsersRepository = new PrismaUsersRepository()
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+
+    return { user }
+  }
+}
